8aabd27ebc8caf3ebaed2c42f251c9bcef90ffb4
magnusdyremoe <magnus.dyremoe@gmail.com>
2020-04-08 14:39:07 +0200

    forslag

---
+++ exercises/project/forlsga.go
@@ -0,0 +1,18 @@
+//Decision
+
+// list of elevator id, position and direction
+//Send list every time <-atFloor
+
+func cost () {
+    //calculate som cost based on the elevators in list
+    //best elev = elev with lowest cost
+    //return best elev
+}
+
+func decision() {
+    //id = cost from function above
+    //if elevID == id {
+        set queue local
+    }
+    else { stay remote}
+}
