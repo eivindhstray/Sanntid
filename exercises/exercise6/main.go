3ae4e9ae6d6a5038e4302bbd85cab5f9e04d1cac
magnusdyremoe <magnus.dyremoe@gmail.com>
2020-04-17 18:02:59 +0200

    correct master slave config. Count not working

--- exercises/exercise6/main.go
+++ exercises/exercise6/main.go
@@ -1,94 +1,95 @@
 package main
 
 import (
-	"os"
 	"fmt"
-	"./Network/network/bcast"
-	"time"
+	"os"
 	"strconv"
+	"time"
+
+	"./Network/network/bcast"
 )
 
+var message msg
 
-
-type msg struct{
-	word string
-	id int
+type msg struct {
+	word   string
+	id     int
 	number int
 }
 
-type alive struct{
+type alive struct {
 	id int
 }
+
 var count int
 
-func main(){
-	localid,err := strconv.Atoi(os.Args[1])
-	if err != nil{
+func main() {
+	localid, err := strconv.Atoi(os.Args[1])
+	if err != nil {
 		fmt.Print("panicyo")
 	}
+	message.id = localid
+
 	tx := make(chan msg)
 	rx := make(chan msg)
 	acktx := make(chan alive)
 	ackrx := make(chan alive)
 	alivemsgtimer := time.NewTimer(0)
-	fmt.Print("id: ", localid, "\n")
-	
+	//fmt.Print("id: ", localid, "\n")
+
 	timeOut := time.NewTimer(0)
 	go bcast.Receiver(15648, rx)
-	go bcast.Transmitter(15648,tx)
-	go bcast.Receiver(15649,ackrx)
-	go bcast.Transmitter(15649,acktx)
-	alivemsgtimer.Reset(200*time.Millisecond)
-	for{
-		select{
-			case receivedmsg:= <- rx:
+	go bcast.Transmitter(15648, tx)
+	go bcast.Receiver(15649, ackrx)
+	go bcast.Transmitter(15649, acktx)
+	alivemsgtimer.Reset(200 * time.Millisecond)
+	for {
+		select {
+		case receivedmsg := <-rx:
 
-				msgid := receivedmsg.id
-				num := receivedmsg.number
-				fmt.Print("msgreceived id:",receivedmsg.id,"\n")
-				switch msgid{
-				case 1:
-					if localid != 1{
-						count = num
-						
-					}
-				
-				default:
+			msgid := receivedmsg.id
+			fmt.Println("idmsg: ", msgid)
+			num := receivedmsg.number
+			//fmt.Print("msgreceived id:", receivedmsg.id, "\n")
+			switch msgid {
+			case 1:
+				if localid != 1 {
+					count = num
 
 				}
 
-			case alivemsg := <- ackrx:
-				fmt.Print(alivemsg.id,"is id\n")
-				if alivemsg.id == localid{
-					timeOut.Reset(1*time.Second)
-				}
-			
-		
-			
-				
-			case <- timeOut.C:
-					localid = 1
-					
-			case <- alivemsgtimer.C:
-				msg := alive{localid}
-				acktx <- msg
-				
-		
+			default:
+
+			}
+
+		case alivemsg := <-ackrx:
+			fmt.Print(alivemsg.id, "is id\n")
+			if alivemsg.id == localid {
+				timeOut.Reset(1 * time.Second)
+			}
+
+		case <-timeOut.C:
+			localid = 1
+
+		case <-alivemsgtimer.C:
+			msg := alive{message.id}
+			acktx <- msg
+
 		}
-		if localid == 1{
+		if message.id == 1 {
 
-			count = count+1
-			fmt.Print("mastercount",count,"\n")
-			msg := msg{"Master",1,count}
-			tx<-msg
-			
-	
-		}else{
-			fmt.Print("slavecount",count,"\n")
-			msg := msg{"Slave",2,0}
+			count = count + 1
+			fmt.Print("mastercount", count, "\n")
+			msg := msg{"Master", 1, count}
+			tx <- msg
+
+		}
+		if message.id == 2 {
+			fmt.Print("slavecount", count, "\n")
+			msg := msg{"Slave", 2, 0}
 			tx <- msg
 		}
-		time.Sleep(1*time.Second)
+		time.Sleep(1 * time.Second)
 
 	}
 }
