48dc8240338deb6e409c58ac02aa6783667691e4
magnusdyremoe <magnus.dyremoe@gmail.com>
2020-04-19 17:31:20 +0200

    decision on floors

--- exercises/project/src/elevator/decisionAlgorithm.go
+++ exercises/project/src/elevator/decisionAlgorithm.go
@@ -23,9 +23,9 @@ func DecisionAlgorithm(buttonPush elevio.ButtonEvent) {
 
 	//Find the remote order and determine cost for every elevator
 	for elevator := 1; elevator < variables.N_ELEVATORS+1; elevator++ {
-		cost := 0
+		//cost := 0
 
-		cost = buttonPush.Floor - Elev.ElevState[elevator][0]
+		cost := buttonPush.Floor - Elev.ElevState[elevator][0]
 		if cost < 0 {
 			cost = -cost
 		}
@@ -33,54 +33,34 @@ func DecisionAlgorithm(buttonPush elevio.ButtonEvent) {
 		correctFloor = buttonPush.Floor
 		correctButton = int(buttonPush.Button)
 
-		if Elev.ElevState[elevator][2] < 0 {
+		if Elev.ElevState[elevator][2] > 0 {
 			cost = cost + variables.ELEV_OFFLINE
 		}
 
 		CostArray[elevator] = cost
 		fmt.Println("Elevator #: ", elevator, "%n Cost: ", cost)
-
-		/*
-			for floors := 0; floors < variables.N_FLOORS; floors++ {
-				for buttons := 0; buttons < variables.N_BUTTON_TYPES-1; buttons++ {
-					if queue[floors][buttons] == variables.REMOTE {
-						cost = floors - Elev.ElevState[elevator][0]
-						if cost < 0 {
-							cost = -cost
-						}
-						correctFloor = floors
-						correctButton = buttons
-
-						if Elev.ElevState[elevator][2] > 0 {
-							cost = cost + variables.ELEV_OFFLINE
-						}
-						CostArray[elevator] = cost
-						fmt.Println("Elevator #: ", elevator, "%n Cost: ", cost)
-					}
-				}
-			}
-		}*/
-
-		//Find best elevator
-		var bestElev int
-		bestElev = 1
-		for elevator := 2; elevator < variables.N_ELEVATORS+1; elevator++ {
-			if CostArray[elevator] < CostArray[bestElev] {
-				bestElev = elevator
-			}
-
-		}
-		fmt.Println("Elevator 1 cost :", CostArray[1])
-		fmt.Println("Elevator 2 cost :", CostArray[2])
-		fmt.Println("Best elevator : ", bestElev)
-		fmt.Println("Elevator ID : ", Elev.ElevID)
-		fmt.Println("*------------------------_*")
-		fmt.Println("Elev 1 at floor: ", Elev.ElevState[1][0])
-		fmt.Println("Elev 2 at floor: ", Elev.ElevState[2][0])
-
-		//Set local in queue of best elevator
-		if bestElev == Elev.ElevID {
-			queue[correctFloor][correctButton] = variables.LOCAL
-		}
 	}
+
+	//Find best elevator
+	var bestElev int
+	bestElev = 1
+	for elevator := 2; elevator < variables.N_ELEVATORS+1; elevator++ {
+		if CostArray[elevator] < CostArray[bestElev] {
+			bestElev = elevator
+		}
+
+	}
+	fmt.Println("Elevator 1 cost :", CostArray[1])
+	fmt.Println("Elevator 2 cost :", CostArray[2])
+	fmt.Println("Best elevator : ", bestElev)
+	fmt.Println("Elevator ID : ", Elev.ElevID)
+	fmt.Println("*------------------------_*")
+	fmt.Println("Elev 1 at floor: ", Elev.ElevState[1][0])
+	fmt.Println("Elev 2 at floor: ", Elev.ElevState[2][0])
+
+	//Set local in queue of best elevator
+	if bestElev == Elev.ElevID {
+		queue[correctFloor][correctButton] = variables.LOCAL
+	}
+
 }
