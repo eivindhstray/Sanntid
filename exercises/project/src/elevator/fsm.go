b97d0fd235497322b85c15abe1671e3e668c897b
Eivind <eivindhstray@gmail.com>
2020-04-19 16:37:03 +0200

    beautified

--- exercises/project/src/elevator/fsm.go
+++ exercises/project/src/elevator/fsm.go
@@ -70,8 +70,9 @@ func FsmMessageReceivedHandler(msg variables.ElevatorMessage, LocalID int) {
 		FsmFloorMessage(floor, ElevDir(dir), msgID)
 	case "FAULTY_MOTOR":
 		ElevatorSetConnectionStatus(variables.NEW_FLOOR_TIMEOUT_PENALTY, msgID)
-		if msgID != LocalID && Elev.Dir == Stop {
-			QueueMakeRemoteLocal()
+		
+		QueueMakeRemoteLocal()
+		if Elev.Dir == Stop {
 			FsmFloor(Elev.CurrentFloor, Elev.Dir)
 		}
 	default:
